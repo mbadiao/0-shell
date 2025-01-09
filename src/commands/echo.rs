f615caa76c8a3e6f0115864ea46765eaefc8bee7
TanakAiko <cherifmbaye02@gmail.com>
2025-01-09 12:32:10 +0000

    echo done with the flag '-n'

--- src/commands/echo.rs
+++ src/commands/echo.rs
@@ -1,3 +1,17 @@
 pub fn echo(args: &[&str]) {
-    println!("COMMAND: echo, ARGS: {:?}", args);
+    let mut skip_newline = false;
+    let mut start_index = 0;
+    
+    if !args.is_empty() && args[0] == "-n" {
+        skip_newline = true;
+        start_index = 1;
+    }
+
+    let output: String = args[start_index..].join(" ");
+
+    if skip_newline {
+        print!("{}", output);
+    } else {
+        println!("{}", output);
+    }
 }
